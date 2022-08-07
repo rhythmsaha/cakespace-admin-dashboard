51a6ef728bcdaabcce7959f71ce3b0c83588e7cf
Rhythm Saha <rs.2001.saha@gmail.com>
2022-08-07 17:12:20 +0530

    1

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,38 @@
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
 function Home() {
-    return <div>Home</div>;
+    const [data, setData] = useState([
+        {
+            x: 10,
+            y: 100,
+        },
+    ]);
+
+    useEffect(() => {
+        data.push({
+            x: 11,
+            y: 500,
+        });
+    }, []);
+
+    const series = [
+        {
+            data: data,
+        },
+    ];
+
+    const options = {
+        stroke: {
+            curve: "smooth",
+        },
+    };
+
+    return (
+        <div>
+            <Chart options={options} series={series} type="line" width={500} height={320} />
+        </div>
+    );
 }
 export default Home;
