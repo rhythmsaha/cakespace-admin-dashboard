d2038533c34d2a7036f20272cd89ef449ef8cf3f
Rhythm Saha <rs.2001.saha@gmail.com>
2022-08-13 23:58:58 +0530

    upload image

--- components/account/UploadImage.js
+++ components/account/UploadImage.js
@@ -1,12 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
+import { motion } from "framer-motion";
 import { useState } from "react";
+import { MdAddAPhoto } from "react-icons/md";
 
 function UploadImage({ user }) {
     const [image, setImage] = useState("");
     const [url, setUrl] = useState(user?.avatar);
+    const [hoverState, setHoverState] = useState(false);
 
-    const uploadImage = () => {
+    const uploadToCloudinary = (file) => {
         const data = new FormData();
-        data.append("file", image);
+        data.append("file", file);
         data.append("upload_preset", "cakespace");
         data.append("cloud_name", "desihzeid");
 
@@ -16,18 +20,51 @@ function UploadImage({ user }) {
         })
             .then((res) => res.json())
             .then((data) => setUrl(data.url))
-            .catch((err) => console.log(err));
+            .catch((err) => {
+                setImage(null);
+            });
+    };
+
+    const createImageBlob = (e) => {
+        if (!e.target.files.length !== 0) {
+            const imageBlob = Object.assign(e.target.files[0], {
+                preview: URL.createObjectURL(e.target.files[0]),
+            });
+
+            setImage(imageBlob.preview);
+        }
     };
 
     return (
-        <div>
-            <label htmlFor="test" className="bg-red-500">
-                <img src={url} className="object-contain h-20 w-20" />
-                <input id="test" type="file" onChange={(e) => setImage(e.target.files[0])} className="hidden" />
+        <div className="flex flex-col items-center justify-center px-4 py-14 md:py-10">
+            <label
+                htmlFor="test"
+                className="cursor-pointer border border-dashed rounded-full p-2 flex items-center justify-center relative h-36 w-36 overflow-hidden"
+                onMouseEnter={() => setHoverState(true)}
+                onMouseLeave={() => setHoverState(false)}
+            >
+                {hoverState && (
+                    <motion.span
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.25, type: "just" }}
+                        className="top-2 bottom-2 left-2 right-2 flex flex-col items-center justify-center gap-2 bg-black bg-opacity-70 rounded-full text-gray-300 absolute "
+                    >
+                        <MdAddAPhoto className="text-2xl " />
+                        <p className="text-xs">Update Photo</p>
+                    </motion.span>
+                )}
+
+                <img src={image || url} className="object-contain h-full w-full rounded-full border" alt="" />
+
+                <input id="test" type="file" onChange={createImageBlob} className="hidden" />
             </label>
-            <button className="p-2 border" onClick={uploadImage}>
-                Upload
-            </button>
+
+            <h2 className="text-sm mt-2 font-bold text-gray-500">Profile Picture</h2>
+
+            <p className="text-center text-xs text-gray-400 font-medium mt-5">
+                Allowed *.jpeg, *.jpg, *.png, *.gif <br /> max size of 3.1 MB
+            </p>
         </div>
     );
 }
--- pages/account/index.js
+++ pages/account/index.js
@@ -11,14 +11,11 @@ function User() {
         <div className="py-4">
             <PageName name="Account Settings" />
 
-            <main className="mt-10 grid md:grid-cols-12 gap-8">
-                <section className="space-y-4 h-72 shadow-0 p-6 rounded-xl bg-white md:col-span-4">
-                    <h1 className="font-semibold text-gray-700">Personal Information</h1>
-                    <div className="">
-                        <UploadImage user={user} />
-                    </div>
+            <main className="mt-10 grid lg:grid-cols-12 gap-8">
+                <section className="space-y-4 shadow-0 p-6 rounded-xl bg-white lg:col-span-4">
+                    <UploadImage user={user} />
                 </section>
-                <section className="space-y-4 h-72 shadow-0 p-6 rounded-xl bg-white md:col-span-8">
+                <section className="space-y-4 h-72 shadow-0 p-6 rounded-xl bg-white lg:col-span-8">
                     <h1 className="font-semibold text-gray-700">Notification</h1>
                     <div className="">
                         <p className="text-[10rem] text-center text-gray-200 font-extralight">X</p>
