8768542725caf8ebdbaabd0da97fe69a43a5709a
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-14 23:49:06 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,69 +5,73 @@ module.exports = withMT({
     darkMode: "class",
     content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {
-            colors: {
-                primary: {
-                    lighter: "#C8FACD",
-                    light: "#5BE584",
-                    main: "#00AB55",
-                    dark: "#007B55",
-                    darker: "#005249",
-                },
+        fontFamily: {
+            sans: ["Public Sans", "sans-serif"],
+        },
 
-                secondery: {
-                    lighter: "#D6E4FF",
-                    light: "#84A9FF",
-                    main: "#3366FF",
-                    dark: "#1939B7",
-                    darker: "#091A7A",
-                },
-
-                info: {
-                    lighter: "#D0F2FF",
-                    light: "#74CAFF",
-                    main: "#1890FF",
-                    dark: "#0C53B7",
-                    darker: "#04297A",
-                },
-
-                success: {
-                    lighter: "#E9FCD4",
-                    light: "#AAF27F",
-                    main: "#54D62C",
-                    dark: "#229A16",
-                    darker: "#08660D",
-                },
-
-                warning: {
-                    lighter: "#FFF7CD",
-                    light: "#FFE16A",
-                    main: "#FFC107",
-                    dark: "#B78103",
-                    darker: "#7A4F01",
-                },
-
-                error: {
-                    lighter: "#FFE7D9",
-                    light: "#FFA48D",
-                    main: "#FF4842",
-                    dark: "#B72136",
-                    darker: "#7A0C2E",
-                },
-
-                grey: {
-                    100: "#F9FAFB",
-                    200: "#F4F6F8",
-                    300: "#DFE3E8",
-                    400: "#C4CDD5",
-                    500: "#919EAB",
-                    600: "#637381",
-                    700: "#454F5B",
-                    800: "#212B36",
-                    900: "#161C24",
-                },
+        colors: {
+            green: {
+                50: "#C8FACD",
+                100: "#5BE584",
+                500: "#00AB55",
+                700: "#007B55",
+                900: "#005249",
             },
 
+            secondery: {
+                lighter: "#D6E4FF",
+                light: "#84A9FF",
+                main: "#3366FF",
+                dark: "#1939B7",
+                darker: "#091A7A",
+            },
+
+            info: {
+                lighter: "#D0F2FF",
+                light: "#74CAFF",
+                main: "#1890FF",
+                dark: "#0C53B7",
+                darker: "#04297A",
+            },
+
+            success: {
+                lighter: "#E9FCD4",
+                light: "#AAF27F",
+                main: "#54D62C",
+                dark: "#229A16",
+                darker: "#08660D",
+            },
+
+            warning: {
+                lighter: "#FFF7CD",
+                light: "#FFE16A",
+                main: "#FFC107",
+                dark: "#B78103",
+                darker: "#7A4F01",
+            },
+
+            error: {
+                lighter: "#FFE7D9",
+                light: "#FFA48D",
+                main: "#FF4842",
+                dark: "#B72136",
+                darker: "#7A0C2E",
+            },
+
+            grey: {
+                100: "#F9FAFB",
+                200: "#F4F6F8",
+                300: "#DFE3E8",
+                400: "#C4CDD5",
+                500: "#919EAB",
+                600: "#637381",
+                700: "#454F5B",
+                800: "#212B36",
+                900: "#161C24",
+            },
+        },
+
+        extend: {
             fontFamily: {
                 "public-sans": ["Public Sans", "sans-serif"],
             },
