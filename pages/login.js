d7dc014bfc47f5373f64f767faf347c31090d620
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-15 01:10:04 +0530

    login

--- pages/login.js
+++ pages/login.js
@@ -47,36 +47,33 @@ export default function Login() {
         try {
             const response = await axios.post(API_URLS.login, { email, password });
 
-            console.log(response);
+            const { JWT_TOKEN, user, message } = await response.data; // Destructureing data from response
 
-            // const { JWT_TOKEN, user, message } = await response.data; // Destructureing data from response
+            toast.success(message); // Show success login success with toast!
 
-            // toast.success(message); // Show success login success with toast!
-
-            // login(JWT_TOKEN, user); // Calls the login method from auth context to update current user state
+            login(JWT_TOKEN, user); // Calls the login method from auth context to update current user state
         } catch (error) {
-            // if (error.type) {
-            //     if (error.type === "error") {
-            //         toast.error(error.message);
-            //     } else {
-            //         setError(error.type, { type: error?.type, message: error.message });
-            //     }
-            // } else {
-            //     if (error.response) toast.error(error.response.data.message);
-            //     else toast.error(error || error.message);
-            // }
+            if (error?.fields && error.fields.length > 0) {
+                error.fields.forEach((field) => {
+                    setError(field.path, { type: field.type, message: field.message });
+                });
+            } else {
+                toast.error(error?.message || error || "Something went wrong!");
+            }
         }
     };
 
     return (
-        <div className="mx-auto w-11/12 max-w-md px-2 pt-[12vh]">
+        <div className="mx-auto w-11/12 max-w-md  pt-[10vh]">
             <div>
                 <img className="mx-auto h-32 object-contain " src="/logo.png" alt="Cakespace" />
-                <Typography variant="h5">Sign in to your account</Typography>
+                <Typography variant="h4" className="font-extrabold px-1 text-xl sm:text-2xl text-center text-gray-700 ">
+                    Sign in to your account
+                </Typography>
             </div>
 
             <form className="mt-8" onSubmit={handleSubmit(submitHandler)}>
-                <div className="space-y-4">
+                <div className="space-y-4 px-2">
                     <Input
                         name="Email"
                         type="text"
@@ -105,18 +102,15 @@ export default function Login() {
                     {!!errors.password && <span className="px-2 text-xs text-red-600">{errors.password?.message}</span>}
                 </div>
 
-                <div className="flex select-none items-center justify-between py-2">
+                <div className="flex justify-between items-center py-2 select-none">
                     <Checkbox label="Remember Me" color="green" {...register("remember")} />
 
-                    <Link
-                        href="#"
-                        className="text-xs font-medium text-green-500 transition  hover:underline sm:text-sm"
-                    >
+                    <Link href="" className="text-xs font-medium text-green-500 transition  hover:underline sm:text-sm">
                         Forgot your password?
                     </Link>
                 </div>
 
-                <div>
+                <div className="px-2">
                     <Button
                         size="md"
                         variant="filled"
