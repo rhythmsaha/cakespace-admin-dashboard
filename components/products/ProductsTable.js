18681cd4122eb0c08810ca7390be09f175dab12b
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-18 09:28:10 +0530

    create products table

--- components/products/ProductsTable.js
+++ components/products/ProductsTable.js
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import { IconButton, Input } from "@material-tailwind/react";
 import { BiSearch } from "react-icons/bi";
 import { BsFilter } from "react-icons/bs";
+import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
 import Card from "../ui/Card";
 
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.min.css";
 
-const ProductsTable = () => {
+const ProductsTable = ({ products = [] }) => {
+    const [page, setPage] = useState(0);
+    const [order, setOrder] = useState("asc");
+    const [selected, setSelected] = useState([]);
+    const [filterName, setFilterName] = useState("");
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [orderBy, setOrderBy] = useState("createdAt");
+    const [productList, setProductList] = useState(products);
+
+    const handleRequestSort = (property) => {
+        const isAsc = orderBy === property && order === "asc";
+        setOrder(isAsc ? "desc" : "asc");
+        setOrderBy(property);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleFilterByName = (filterName) => {
+        setFilterName(filterName);
+    };
+
+    const handleDeleteProduct = (productId) => {
+        const deleteProduct = productList.filter((product) => product.id !== productId);
+        setSelected([]);
+        setProductList(deleteProduct);
+    };
+
     return (
         <Card>
-            <div>
+            <div className="select-none">
                 <header className="flex items-center justify-between gap-6">
                     <div className="max-w-xs">
                         <Input size="lg" label="Search" icon={<BiSearch />} />
@@ -26,14 +57,31 @@ const ProductsTable = () => {
                             <thead className="">
                                 <tr className="bg-grey-200">
                                     <th className="min-w-[10rem] whitespace-nowrap py-5 px-4 text-left text-xs text-gray-700 first:rounded-l-md last:rounded-r-md sm:text-sm lg:text-base">
-                                        Product
+                                        <div
+                                            className="flex cursor-pointer items-center gap-2"
+                                            onClick={() => {
+                                                setOrderBy("product");
+                                                if (order === "asc") {
+                                                    setOrder("desc");
+                                                } else {
+                                                    setOrder("asc");
+                                                }
+                                            }}
+                                        >
+                                            <span>Product</span>
+                                            {orderBy === "product" &&
+                                                (order === "asc" ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />)}
+                                        </div>
                                     </th>
+
                                     <th className="whitespace-nowrap py-5 px-4 text-left text-xs text-gray-700 first:rounded-l-md last:rounded-r-md sm:text-sm lg:text-base">
                                         Created At
                                     </th>
+
                                     <th className="whitespace-nowrap py-5 px-4 text-center text-xs text-gray-700 first:rounded-l-md last:rounded-r-md sm:text-sm lg:text-base">
                                         Status
                                     </th>
+
                                     <th className="whitespace-nowrap py-5 px-4 text-center text-xs text-gray-700 first:rounded-l-md last:rounded-r-md sm:text-sm lg:text-base">
                                         Price
                                     </th>
